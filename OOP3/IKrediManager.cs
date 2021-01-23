38a3023ecda45acf15f7abab581d5a8968f0a374
ENES NISANCI <2160656017@nku.edu.tr>
2021-01-23 23:54:12 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{   //operasyonları tanımlıyoruz...
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {   // interface onu implemente eden class'ın referansını tutabilir.
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, fileLoggerService);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
